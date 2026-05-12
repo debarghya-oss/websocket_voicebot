6ae7ce51575b6adda5d66b2cc60d10d9300e4cbd
rohan <rohan@hivyr.ai>
2026-05-12 17:04:23 +0530

    uv support added

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# to be run on ubuntu 24.04+ 
+
+#get system up to date
+sudo apt update
+sudo apt install -y software-properties-common
+
+# add python repo for getting python 3.12
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
+#install python 3.12 and venv and dev packages
+sudo apt install -y python3.12 python3.12-venv python3.12-dev
